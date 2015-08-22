ac5c8ab2685828f4ac718d18fc288bf02b13d28a
Hiromitsuuuuuu <hiromitsuuuuu@gmail.com>
2015-08-22 15:52:42 +0900

    [modify] add my answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,34 @@
 class BonusDrink
+
+    @@total = 0
+
+
   def self.total_count_for(amount)
-    0
+
+    @@total = amount
+    get_bonus(amount)
+
   end
+
+
+  def self.get_bonus(amount)
+
+    bonus = (amount/3).to_i
+
+    if amount >= 3
+
+        @@total += bonus
+        rest = bonus + amount % 3
+        get_bonus(rest)
+
+    else
+
+        return @@total
+
+    end
+
+  end
+
 end
+
+p BonusDrink.total_count_for(100)
